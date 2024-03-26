0878921e6a3093a29c2cbed9e53a2ab341073ffc
Arfeen Khan <155321957+ArfeenKhan12@users.noreply.github.com>
2024-03-27 00:39:47 +0500

    For loop assignment Created

---
+++ For-loop-assignment/app.js
@@ -0,0 +1,216 @@
+// QUESTION NO 1
+
+// const msg = "Hello World"
+// const ul = document.querySelector('ul')
+
+// for(let i = 0 ; i < 5 ; i++){
+//     ul.innerHTML +=`<li>${msg} ${i}</li>`
+// }
+
+// QUESTION NO 2
+
+// for(i = 0; i <= 10 ; i++){
+//     console.log(i)
+// }
+
+// QUESTION NO 3
+
+// let num = +prompt('Enter Your Number')
+// let length = +prompt('Enter the length of table')
+
+// for( num = length ; num <= length ; num++){
+//     for(let i= 1; i <= length ; i++ ){
+//         console.log(`${num} X ${i} = ${num*i}`)
+//     }
+// }
+
+// QUESTION NO 4
+
+// const mobileName = ['Nokia' , 'Samsung' , 'Apple' , 'Sony' , 'Huawei']
+// const ul = document.querySelector('ul')
+
+// for ( let i = 0 ; i < mobileName.length ; i++ ){
+//     ul.innerHTML += `<li>${mobileName[i]}</li>`
+// }
+
+
+// QUESTION NO 5
+
+// fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+
+// let ul = document.querySelector('#orderlist');
+// let paragraph = document.querySelector('#orderlist')
+
+// for (let i = 0; i < fruits.length; i++) {
+//     ul.innerHTML += `<li>Element at index ${i} ${fruits[i]}</li>`
+// }
+
+// QUESTION NO 6
+
+
+
+// QUESTION NO 7
+
+
+// let h1 = document.querySelector('h1')
+// let h2 = document.querySelector('h2')
+// let h3 = document.querySelector('h3')
+// let h4 = document.querySelector('h4')
+// let h5 = document.querySelector('h5')
+
+
+
+
+// for ( i = 1 ; i <= 10 ; i ++){
+//     h1.innerHTML += `${i}` 
+// }
+
+
+// for(let i = 10; i >= 1; i--){
+//     h2.innerHTML += `${i}`
+// }
+
+
+// for(let i = 2; i <= 20; i += 2){
+//     h3.innerHTML += `${i}`
+// }
+
+
+// for (let i = 3; i <= 20; i += 3){
+//     h4.innerHTML += `${i}`
+// }
+
+
+// for(let i = 2; i <= 20; i += 2){
+//     h5.innerHTML += `${i + 'k'}` 
+// }
+
+
+// QUESTION NO 8
+
+// let bakery = ["cake", "apple pie", "cookie", "chips", "patties"];
+// let userInput = prompt('Enter Your Items Name');
+
+// for(let i = 0; i < bakery.length; i++){
+//     if(userInput === bakery[i]){
+//         console.log('Its Available')
+//     }
+// }
+// if ( bakery == userInput){
+//     console.log('Its  Available');
+// }
+// else('not available')
+
+// QUESTION NO 9
+
+
+// QUESTION NO 10
+
+
+
+// QUESTION NO 11
+
+
+
+// QUESTION NO 12
+
+// for( let i = 5 ; i <= 100 ; i += 5){
+//     console.log(i);
+// }
+
+
+// QUESTION NO 13
+
+// const table = document.querySelector('#table')
+// const student = ["Ali", "Sami", "Taha", "Inam"];
+// const scores = [58, 73, 89, 90];
+
+// for (let i = 0; i < student.length; i++) {
+//     table.innerHTML += `<table>
+//     <tr>
+//     <th>Student</th>
+//     <th>Score</th>
+//     </tr>
+//     <td>${student[i]}</td>
+//     <td>${scores[i]}</td>
+
+//     </table>`
+// }
+
+// QUESTION NO 14
+
+
+
+// QUESTION NO 15
+
+// const ul = document.querySelector('ul')
+// const nested = [ [1,2,3] , [4,5,6] , [7,8,9] ]
+
+// for (let i = 0; i < nested.length; i++){
+//     console.log(nested[i]);
+//     ul.innerHTML += `<li>
+//     ${nested[i]}
+//     </li>`
+// }
+
+// QUESTION NO 16
+
+// let h1 = document.querySelector('#h1')
+// let input = +prompt('Enter your Number')
+
+// for (input = 0 ; input > 0 ; num -= 0.5){
+//     h1.innerHTML += `${input}` 
+// }
+
+// QUESTION NO 17
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(`${i} is Even`);
+//     }else{
+//         console.log(`${i} is Odd`);
+//     }
+// }
+
+
+// QUESTION NO 18
+
+// let product = 1;
+
+// for (let i = 1; i <= 7; i++) {
+//     if (i % 2 !== 0) {
+//         product *= i;
+//     }
+// }
+// console.log("Product of odd integers from 1 to 7:", product);
+
+// QUESTION NO 19
+
+// const num = 10;
+// for (let i = num; i >= 1; i--) {
+//     const emptystr = '';
+//     for (let j = 0; j < i; j++) {
+//         emptystr += '*'
+//     }
+//     console.log(emptystr);
+// }
+
+// QUESTION NO 20
+
+// const num = 10;
+// const emptystr = ''
+
+// for (let i = 0 ; i = num ; i+= num){
+//     emptystr += '*';
+//     console.log(emptystr);
+// }
+
+// for (let i = 0; i < num; i++) {
+//     emptystr += '*';
+//     console.log(emptystr);
+// }
+
+// for (let i = 0; i > num; i++) {
+//     emptystr += '*';
+//     console.log(emptystr);
+// }
